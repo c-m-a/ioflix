813751d985ec46f4bf862902918eb383caa97070
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-06-29 13:55:22 -0500

    Add: Static files and root configuration to settings

--- core/settings.py
+++ core/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR.parent / 'ioflix-cdn' / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
